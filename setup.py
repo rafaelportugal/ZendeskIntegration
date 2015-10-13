4f1143a089f14deb1949ca12c7dddfe82be98791
Rafael Portugal <rportugal@stone.com.br>
2015-10-13 00:30:58 -0300

    Inserindo o requirements e ajeitando as dependencias

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(name="zendesk",
       cmdclass={'test': PyTest},
 
       # TODO: List of packages that this one depends upon:
-      install_requires=['requests==2.5.1', 'inflect==0.2.5'],
+      install_requires=['requests==2.5.1', 'inflection==0.3.1', 'mock'],
       # TODO: List executable scripts, provided by the package (this is just an example)
       entry_points={
         'console_scripts':
